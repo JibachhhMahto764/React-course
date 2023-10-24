b597a6d2c3fb59f97137ac4e99b4eb0dde206913
jibachh <jibachhmahato@gmail.com>
2023-10-24 17:52:10 +0545

    adding all files

---
+++ index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(<h1>hello jibachh singh</h1>, document.getElementById("h1"));
+ReactDOM.render(<p> hii, my name is jibachh singh!</p>,document.getElementById("h1"))
+ReactDOM.render(<ul>
+    <li>
+        jibachh
+    </li>
+    <li>
+        ramjiban
+    </li>
+</ul>,document.getElementById("h1"))
