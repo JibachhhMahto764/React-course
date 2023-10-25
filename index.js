abfb42188e7a2e4743aac6311408fdb8e689e1cc
jibachh <jibachhmahato@gmail.com>
2023-10-25 11:56:19 +0545

    feat: adding navbar

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-ReactDOM.render(<h1>hello jibachh singh</h1>, document.getElementById("h1"));
-ReactDOM.render(<p> hii, my name is jibachh singh!</p>,document.getElementById("h1"))
-ReactDOM.render(<ul>
-    <li>
-        jibachh
-    </li>
-    <li>
-        ramjiban
-    </li>
-</ul>,document.getElementById("h1"))
+ReactDOM.render(
+    <div>
+        <navbar />
+        <Maincostomecomponent />
+    </div>,
+    document.getElementById("h1")
+
+)
